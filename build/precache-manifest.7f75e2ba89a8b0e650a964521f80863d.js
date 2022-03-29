97667f7a9a14d47b2763e6df10f1fbe736eb26e2
Jorge Andres Padilla <jandrespadilla@gmail.com>
2022-03-29 20:53:41 -0300

    Subo el build

---
+++ build/precache-manifest.7f75e2ba89a8b0e650a964521f80863d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c85a4aa50baa05f072c",
+    "url": "/static/css/main.bd4bfb7e.chunk.css"
+  },
+  {
+    "revision": "0c85a4aa50baa05f072c",
+    "url": "/static/js/main.0c85a4aa.chunk.js"
+  },
+  {
+    "revision": "e0859061d56b1334f7b4",
+    "url": "/static/css/1.cf629a6f.chunk.css"
+  },
+  {
+    "revision": "e0859061d56b1334f7b4",
+    "url": "/static/js/1.e0859061.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f53e15942219648e6cae6d1d74d31c13",
+    "url": "/static/media/carro.f53e1594.png"
+  },
+  {
+    "revision": "c36b591d09d587338a90557d310776d6",
+    "url": "/index.html"
+  }
+];
